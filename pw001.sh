d08f48d2d7c43ce5288c7b43fd9447b92f44e47f
layen67 <lkbcontact@gmail.com>
2020-03-09 23:31:01 +0100

    Update pw001.sh

--- pw001.sh
+++ pw001.sh
@@ -290,6 +290,8 @@ sed -i -r "s/.*tls_private_key_path.*/  tls_private_key_path: \/var\/lib\/docker
 sed -i -r "s/.*postal.cert.*/    ssl_certificate          \/var\/lib\/docker\/wordpress\/ssl_certs\/postal.$1\/production\/signed.crt;/g" /etc/nginx/sites-available/default;
 sed -i -r "s/.*postal.key.*/    ssl_certificate_key      \/var\/lib\/docker\/wordpress\/ssl_certs\/postal.$1\/production\/domain.key;/g" /etc/nginx/sites-available/default;
 
+free -h && sudo sysctl vm.drop_caches=3 && free -h
+
 docker-compose up -d;
 sleep 120
 docker-compose stop;
