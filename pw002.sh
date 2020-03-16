c79b343420bdd9485d3e2a304aa086963ee93378
layen67 <lkbcontact@gmail.com>
2020-03-16 13:41:21 +0100

    Update pw002.sh

--- pw002.sh
+++ pw002.sh
@@ -170,18 +170,20 @@ service nginx restart;
 #
 # Dependencies
 #
+apt-get update;
 sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
+    gnupg-agent \
     software-properties-common
 
 curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
 
 sudo add-apt-repository \
-    "deb https://apt.dockerproject.org/repo/ \
-    ubuntu-$(lsb_release -cs) \
-    main"
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 
 sudo apt-get update;
 sudo apt-get -y install docker-engine docker-compose;
