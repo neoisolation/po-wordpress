16e56438b4f7c2a9e4c2530f3c3999d2791f2746
layen67 <lkbcontact@gmail.com>
2020-03-16 17:00:09 +0100

    Update pw002.sh

--- pw002.sh
+++ pw002.sh
@@ -171,22 +171,10 @@ service nginx restart;
 # Dependencies
 #
 apt-get update;
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
+apt-get install -y docker.io;
+curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose;
+chmod +x /usr/local/bin/docker-compose;
 
-curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
-
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
-sudo apt-get update;
-sudo apt-get -y install docker-engine docker-compose;
 
 # add current user to docker group so there is no need to use sudo when running docker
 sudo usermod -aG docker $(whoami);
