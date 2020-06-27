3bbe0052fe36193f3a7f5a94cab325945f3feedf
layen67 <lkbcontact@gmail.com>
2020-06-27 03:15:47 +0200

    Update frenomcname.sh

--- frenomcname.sh
+++ frenomcname.sh
@@ -5,6 +5,17 @@ frenomlogin=$2;
 frenompass=$3;
 pmta=$4
 RANDOM=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+RANDOM1=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+RANDOM2=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+RANDOM3=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+RANDOM4=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+RANDOM5=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+RANDOM6=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+RANDOM7=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+RANDOM8=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+RANDOM9=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+RANDOM10=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+RANDOM11=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 
 echo "
 login: $2
@@ -18,57 +29,57 @@ record:
     target: $4 # you can omit this line
   # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
-    name: $RANDOM
+    name: $RANDOM1
     type: CNAME
     target: $4 # you can omit this line
       # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
-    name: $RANDOM
+    name: $RANDOM2
     type: CNAME
     target: $4 # you can omit this line
   # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
-    name: $RANDOM
+    name: $RANDOM3
     type: CNAME
     target: $4 # you can omit this line
       # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
-    name: $RANDOM
+    name: $RANDOM4
     type: CNAME
     target: $4 # you can omit this line
   # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
-    name: $RANDOM
+    name: $RANDOM5
     type: CNAME
     target: $4 # you can omit this line
       # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
-    name: $RANDOM
+    name: $RANDOM6
     type: CNAME
     target: $4 # you can omit this line
   # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
-    name: $RANDOM
+    name: $RANDOM7
     type: CNAME
     target: $4 # you can omit this line
       # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
-    name: $RANDOM
+    name: $RANDOM8
     type: CNAME
     target: $4 # you can omit this line
   # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
-    name: $RANDOM
+    name: $RANDOM9
     type: CNAME
     target: $4 # you can omit this line
       # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
-    name: $RANDOM
+    name: $RANDOM10
     type: CNAME
     target: $4 # you can omit this line
   # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
-    name: $RANDOM
+    name: $RANDOM11
     type: CNAME
     target: $4 # you can omit this line
 "> /etc/cnamefreenom.yml;
