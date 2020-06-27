01994dfba3aa5dc99c724515321f8f09cea4c596
layen67 <lkbcontact@gmail.com>
2020-06-27 03:12:24 +0200

    Update frenomcname.sh

--- frenomcname.sh
+++ frenomcname.sh
@@ -3,6 +3,8 @@
 domaine=$1;
 frenomlogin=$2;
 frenompass=$3;
+pmta=$4
+RANDOM=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 
 echo "
 login: $2
@@ -13,61 +15,61 @@ record:
   - domain: $1
     name: $RANDOM
     type: CNAME
-    target: $1 # you can omit this line
+    target: $4 # you can omit this line
   # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
     name: $RANDOM
     type: CNAME
-    target: $1 # you can omit this line
+    target: $4 # you can omit this line
       # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
     name: $RANDOM
     type: CNAME
-    target: $1 # you can omit this line
+    target: $4 # you can omit this line
   # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
     name: $RANDOM
     type: CNAME
-    target: $1 # you can omit this line
+    target: $4 # you can omit this line
       # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
     name: $RANDOM
     type: CNAME
-    target: $1 # you can omit this line
+    target: $4 # you can omit this line
   # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
     name: $RANDOM
     type: CNAME
-    target: $1 # you can omit this line
+    target: $4 # you can omit this line
       # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
     name: $RANDOM
     type: CNAME
-    target: $1 # you can omit this line
+    target: $4 # you can omit this line
   # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
     name: $RANDOM
     type: CNAME
-    target: $1 # you can omit this line
+    target: $4 # you can omit this line
       # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
     name: $RANDOM
     type: CNAME
-    target: $1 # you can omit this line
+    target: $4 # you can omit this line
   # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
     name: $RANDOM
     type: CNAME
-    target: $1 # you can omit this line
+    target: $4 # you can omit this line
       # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
     name: $RANDOM
     type: CNAME
-    target: $1 # you can omit this line
+    target: $4 # you can omit this line
   # the following will update your subdomain's A record with your current ip (v4)
   - domain: $1
     name: $RANDOM
     type: CNAME
-    target: $1 # you can omit this line
+    target: $4 # you can omit this line
 "> /etc/cnamefreenom.yml;
 fdu process -c -i -t 3600 /etc/cnamefreenom.yml&
