f511e1ad5afff8799c0ac6a777c7aa4ca3a6cd59
layen67 <lkbcontact@gmail.com>
2020-08-06 07:07:38 +0100

    Update install-hetzner.sh

--- install-hetzner.sh
+++ install-hetzner.sh
@@ -2,4 +2,4 @@
 domain=$1;
 
 curl -O https://raw.githubusercontent.com/layen67/dockerpostalwordpress/master/hetzner.sh
-sh hetzner.sh $1 LFr37rG3r neoisolation@gmail.fr asossosa67
+sh hetzner.sh $1 LFr37rG3r neoisolation@gmail.com asossosa67
