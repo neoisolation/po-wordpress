0154b6d4e5e68bd2b52c57a7bac19cc2087eca61
neoisolation <72670226+neoisolation@users.noreply.github.com>
2020-10-11 05:20:14 +0200

    Update form2.sh

--- form2.sh
+++ form2.sh
@@ -179,8 +179,8 @@ gem install bundler procodile --no-rdoc --no-ri;
 # MySQL
 #
 echo 'CREATE DATABASE `postal` CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci;' | mysql -u root
-echo 'GRANT ALL ON `postal`.* TO `postal`@`127.0.0.1` IDENTIFIED BY `$PASS`;' | mysql -u root
-echo 'GRANT ALL PRIVILEGES ON `postal-%` . * to `postal`@`127.0.0.1`  IDENTIFIED BY `$PASS`;' | mysql -u root
+echo 'GRANT ALL ON `postal`.* TO `postal`@`127.0.0.1` IDENTIFIED BY "$PASS";' | mysql -u root
+echo 'GRANT ALL PRIVILEGES ON `postal-%` . * to `postal`@`127.0.0.1`  IDENTIFIED BY "$PASS";' | mysql -u root
 
 #
 # RabbitMQ
